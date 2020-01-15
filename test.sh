ec3de668ac8fe52f4ba25eb01d1473d3d1a00c21
Mike Vartanian <Mike.Vartanian@nih.gov>
2020-01-15 15:38:34 -0500

    Name container after git hash

--- test.sh
+++ test.sh
@@ -2,9 +2,16 @@
 set -euo pipefail
 shopt -s nullglob globstar
 
-env
+if [[ -z ${BRANCH_NAME+x} ]]; then
+    BRANCH_NAME="none"
+fi
 
-docker run --name ga4gh -p 443:80 drs
+if [[ -z ${GIT_COMMIT+x} ]]; then
+    GIT_COMMIT="none"
+fi
+
+#docker run --name ga4gh -p 443:80 drs
+docker run -t "${BRANCH_NAME}_${GIT_COMMIT:0:6}" -p 443:80 drs
 # http://.../wsgi -> "Hello World!"
 
 exit 0
