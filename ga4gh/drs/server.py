bd3de90d09edd557dd87c87d29356af4c2891e2a
Kenneth Durbrow <kmdurbrow@me.com>
2020-01-30 14:48:16 -0500

    Comments and cleanup

--- ga4gh/drs/server.py
+++ ga4gh/drs/server.py
@@ -72,9 +72,11 @@ def apikey_auth(token, required_scopes):
     return ok
 
 def _ParseObjectId(objId: str):
+    """ Split up an object id """
     type = None
     vers = None
     if objId:
+        m_id = re.search(r'^([^.]+)(.*)', objId)
         mtype = re.search(r'\.([^.]+)$', objId)
         if mtype:
             type = mtype[1]
@@ -87,7 +89,14 @@ def _ParseObjectId(objId: str):
             (type, vers) = (None, type)
     return (objId, vers, type)
 
+### @brief _ParseSDLResponseV2 Parse version 2 SDL response JSON
+###
+### @param response: SDL response JSON
+### @param query: { bundle: accession, type: file type }
+###
+### @return None if error else Dictionary with requested object or a 404
 def _ParseSDLResponseV2(response, query):
+    """ Parse version 2 SDL Response JSON """
     results = response.get('result')
     if type(results) != list:
         logging.error('unexpected result ' + results)
@@ -97,7 +106,6 @@ def _ParseSDLResponseV2(response, query):
         bundle = result.get('bundle')
         status = result.get('status')
         if not bundle or not status:
-            logging.error('unexpected result ' + result)
             return None
 
         status = str(status)
@@ -107,7 +115,6 @@ def _ParseSDLResponseV2(response, query):
 
             files = result.get('files')
             if type(files) != list:
-                logging.error('unexpected files ' + files)
                 return None
 
             for file in files:
@@ -115,12 +122,9 @@ def _ParseSDLResponseV2(response, query):
                 if query['type'] in filetype:
                     locations = file.get('locations')
                     if type(locations) != list:
-                        logging.error('unexpected locations ' + locations)
                         return None
 
                     for location in locations:
-                        service = location.get('service')
-                        region = location.get('region')
                         return {'status': '200'
                               , 'msg': result.get('msg')
                               , 'name': file.get('name')
@@ -133,9 +137,16 @@ def _ParseSDLResponseV2(response, query):
                               , 'region': location.get('region')
                               }
 
-    return { 'status': 404, 'msg': 'not found' }
+    return { 'status': '404', 'msg': 'not found' }
 
+### @brief _ParseSDLResponse Parse SDL response JSON
+###
+### @param response: SDL response JSON
+### @param query: { bundle: accession, type: file type }
+###
+### @return None if error else Dictionary
 def _ParseSDLResponse(response, query):
+    """ Parse SDL Response JSON """
     version = response.get('version')
     if version == '2':
         return _ParseSDLResponseV2(response, query)
@@ -146,6 +157,18 @@ def _ParseSDLResponse(response, query):
 def _GetRedirURL(url: str, service: str, region: str):
     return url
 
+def _GetAccessMethod(res):
+    """ Create a DRS Access Method from SDL info """
+    access_method = { 'access_url': _GetRedirURL(res['url'], res['service'], res['region']) }
+    if res['service'] == 's3': access_method['type'] = 's3'
+    elif res['service'] == 'gs': access_method['type'] = 'gs'
+    else access_method['type'] = 'https'
+    if res['region']: access_method['region'] = res['region']
+    return access_method
+
+def _GetComputeEnvironmentToken():
+    return None
+
 def GetObject(object_id: str, expand: bool):
     logging.info(f"In GetObject {object_id} {expand}")
     logging.info(f"params is {connexion.request.json}")
@@ -155,36 +178,37 @@ def GetObject(object_id: str, expand: bool):
           , 'self_uri': connexion.request.url ###< this might not be right
           }
 
-    # TODO: Confirm object_id matches [A-Za-z0-9.-_~]+
+    # ???: Confirm object_id matches [A-Za-z0-9.-_~]+
     (acc, vers, type) = _ParseObjectId('SRR10039049.bam') # _ParseObjectId(object_id)
     params = {'accept-proto': 'https'}
     params['acc'] = acc + '.' + vers if acc and vers else acc else ''
     params['filetype'] = type if type else 'bam'
 
-    sdl = requests.post(SDL_RETRIEVE_CGI, data=params)
+    hdrs = {}
+    cet = _GetComputeEnvironmentToken()
+    if cet: hdrs{'ident'} = cet
+
+    sdl = requests.post(SDL_RETRIEVE_CGI, data=params, headers=hdrs)
     ret["sdl_status"] = sdl.status_code
     ret["sdl_json"] = sdl.json()
 
     res = _ParseSDLResponse(sdl.json(), {'bundle': params['acc'], 'type': params['filetype']})
-    if res and res['status'] == '200':
-        # required fields
-
+    if not res:
+        logging.error('unexpected response ' + sdl.json())
+    elif res['status'] == '200':
+        # MARK: required fields
         ret['checksums'] = [{ 'checksum': res['md5'], 'type': 'md5' }]
         ret['size'] = res['size']
         ret['created_time'] = res['date']
 
-        # optional fields
+        # MARK: optional fields
         if res['name']: ret['name'] = res['name']
         if vers: ret['version'] = vers
-        access_method = { 'access_url': _GetRedirURL(res['url'], res['service'], res['region']) }
-        if res['service'] == 's3': access_method['type'] = 's3'
-        elif res['service'] == 'gs': access_method['type'] = 'gs'
-        else access_method['type'] = 'https'
-        if res['region']: access_method['region'] = res['region']
-        ret['access_methods'] = [ access_method ]
+
+        # NOTE: this is were the URL is stored
+        ret['access_methods'] = [ _GetAccessMethod(res) ]
 
     return ret
-
 
 def GetAccessURL(object_id: str, access_id: str):
     logging.info(f"In GetAccessURL {object_id} {access_id}")
