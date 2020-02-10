64fa5e8a2b45eb277a17f1ffe3aaf012887e2022
Kenneth Durbrow <kmdurbrow@me.com>
2020-02-10 13:53:49 -0500

    docker-ize the unit tests script

---
+++ ut_mini.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+shopt -s nullglob globstar
+
+LOG="/tmp/uwsgi_$USER.log"
+rm -f "$LOG"
+
+# echo "Starting uwsgi"
+UWSGI=`which uwsgi`
+echo "Starting uwsgi ($UWSGI)"
+
+PORT=$((RANDOM+1024))
+#~/.local/bin/uwsgi --http ":$PORT" --wsgi-file drs.py &
+echo ${UWSGI} --logto "$LOG" --http ":$PORT" --wsgi-file drs.py
+${UWSGI} --logto "$LOG" --http ":$PORT" --wsgi-file drs.py & sleep 2
+
+RET=0
+
+out=$(curl -s -H 'Authorization: authme' http://localhost:$PORT/ga4gh/drs/v1/objects/SRR000000.f4.m.liv.DMSO1.rna.merged.sorted.bam | jq -S '.')
+if [[ "$out" =~ "02b1ea5174fee52d14195fd07ece176a" ]]; then
+    echo "OK results were: $out"
+else
+    echo "Test failed: $out"
+    RET=1
+fi
+
+echo "Stopping uwsgi"
+kill %1
+
+# Run mock server
+# connexion run openapi/data_repository_service.swagger.yaml --mock=all -v
+
+if [[ "$RET" -eq 0 ]]; then
+    echo "Unit tests complete"
+    exit 0
+fi
+
+echo "See $LOG for details"
+cat $LOG
+exit 1
