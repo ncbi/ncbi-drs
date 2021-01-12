0e9abc6da4539835011cf226f43ea012e6bb8e0b
Kenneth Durbrow <kmdurbrow@me.com>
2021-01-12 12:46:46 -0500

    Reset repo

--- source/unit-test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker run -v `pwd`:/source:ro --rm `docker build -qq .`
--- tests/basic_test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# r=requests.get(...)
-def test():
-    pass
-
-
-def test2():
-    assert True
